badc63a8fce3de0b7f671aa54af9b4820c388590
Dev- Darey <ojeksdarey@gmail.com>
2021-01-11 11:08:14 +0100

    Added the color component

---
+++ color-app/src/ColorBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class ColorBox extends Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default ColorBox;
---
+++ color-app/src/Palette.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+class Palette extends Component {
+    state = {}
+    render() {
+        return (
+
+            < div className="Palette">
+                {/* Navbar goes here */}
+                <div className="Palette-colors">
+                    {/* bunch of colors */}
+                </div>
+                {/** Footer */}
+                <div>
+
+                </div>
+                <h1>Palette</h1>
+            </div >
+        );
+    }
+}
+
+export default Palette;
